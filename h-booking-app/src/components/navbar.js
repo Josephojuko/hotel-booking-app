fb4fdfd97637b33c03f60bd1171cf1142fef5928
Josephojuko <122806263+Josephojuko@users.noreply.github.com>
2024-04-19 10:26:26 +0000

    installing tailwind css

---
+++ h-booking-app/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar () {
+    return (
+        <div className="text-yellow-700">
+            <span>Home</span>
+            <span>About us</span>
+            <span>Contact us</span>
+        </div>
+    )
+}
+export default Navbar;
